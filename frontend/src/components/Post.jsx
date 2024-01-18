12762fd8d7d764520c1bca4aaa5e5e59dc688919
geeky-amit <singh.amit_007@hotmail>
2024-01-18 09:51:58 +0530

    .

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -23,35 +23,47 @@ const Post = () => {
                     </div>
                 </div>
                 <div className="post-card">
-                    <img src={"https://www.thebalancemoney.com/thmb/OqDqeT4W7DUsGBEas4kAgSxm8QM=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Blogging-2227d2f697a54b6395f966dd0d85db6c.jpg"} />
+                    <img className="post-img" src={"https://www.thebalancemoney.com/thmb/OqDqeT4W7DUsGBEas4kAgSxm8QM=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Blogging-2227d2f697a54b6395f966dd0d85db6c.jpg"} />
                     <h2>Automation In Agriculture With Mobile App Development</h2>
                     <div className="author">
-                        <span>Amit Patel</span>
-                        <button>read full article</button>
+                        <div className="author-name">
+                            <img style={{ height: "18px", width: "18px", paddingTop: "2px" }} src={userIcon} alt="icon-user" />
+                            <span>Amit Patel</span>
+                        </div>
+                        <button className="btn-article" onClick={() => navigate("/singlepost")} >read full article</button>
                     </div>
                 </div>
                 <div className="post-card">
-                    <img src={"https://www.thebalancemoney.com/thmb/OqDqeT4W7DUsGBEas4kAgSxm8QM=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Blogging-2227d2f697a54b6395f966dd0d85db6c.jpg"} />
+                    <img className="post-img" src={"https://www.thebalancemoney.com/thmb/OqDqeT4W7DUsGBEas4kAgSxm8QM=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Blogging-2227d2f697a54b6395f966dd0d85db6c.jpg"} />
                     <h2>Automation In Agriculture With Mobile App Development</h2>
                     <div className="author">
-                        <span>Amit Patel</span>
-                        <button>read full article</button>
+                        <div className="author-name">
+                            <img style={{ height: "18px", width: "18px", paddingTop: "2px" }} src={userIcon} alt="icon-user" />
+                            <span>Amit Patel</span>
+                        </div>
+                        <button className="btn-article" onClick={() => navigate("/singlepost")} >read full article</button>
                     </div>
                 </div>
                 <div className="post-card">
-                    <img src={"https://www.thebalancemoney.com/thmb/OqDqeT4W7DUsGBEas4kAgSxm8QM=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Blogging-2227d2f697a54b6395f966dd0d85db6c.jpg"} />
+                    <img className="post-img" src={"https://www.thebalancemoney.com/thmb/OqDqeT4W7DUsGBEas4kAgSxm8QM=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Blogging-2227d2f697a54b6395f966dd0d85db6c.jpg"} />
                     <h2>Automation In Agriculture With Mobile App Development</h2>
                     <div className="author">
-                        <span>Amit Patel</span>
-                        <button>read full article</button>
+                        <div className="author-name">
+                            <img style={{ height: "18px", width: "18px", paddingTop: "2px" }} src={userIcon} alt="icon-user" />
+                            <span>Amit Patel</span>
+                        </div>
+                        <button className="btn-article" onClick={() => navigate("/singlepost")} >read full article</button>
                     </div>
                 </div>
                 <div className="post-card">
-                    <img src={"https://www.thebalancemoney.com/thmb/OqDqeT4W7DUsGBEas4kAgSxm8QM=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Blogging-2227d2f697a54b6395f966dd0d85db6c.jpg"} />
+                    <img className="post-img" src={"https://www.thebalancemoney.com/thmb/OqDqeT4W7DUsGBEas4kAgSxm8QM=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Blogging-2227d2f697a54b6395f966dd0d85db6c.jpg"} />
                     <h2>Automation In Agriculture With Mobile App Development</h2>
                     <div className="author">
-                        <span>Amit Patel</span>
-                        <button>read full article</button>
+                        <div className="author-name">
+                            <img style={{ height: "18px", width: "18px", paddingTop: "2px" }} src={userIcon} alt="icon-user" />
+                            <span>Amit Patel</span>
+                        </div>
+                        <button className="btn-article" onClick={() => navigate("/singlepost")} >read full article</button>
                     </div>
                 </div>
             </div>
